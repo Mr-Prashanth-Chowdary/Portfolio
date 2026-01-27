6b997081b5a1cf9e955ee45caa6256d444f2c278
Prashanth Chowdary <prashanth.chowdary@webileapps.com>
2026-01-27 17:08:27 +0530

    SEO

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,6 @@ import Title from './components/Title'
 export default function App() {
   return (
     <div className="min-h-screen flex flex-col justify-between p-4">
-      <h1>Prashanth Chowdary</h1>
       <Nav />
       <Title />
       <HeroSection />
