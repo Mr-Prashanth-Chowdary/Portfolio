7f7240d1e390f045ee5b853a87865cfc126604e9
Prashanth Chowdary <prashanth.chowdary@webileapps.com>
2026-01-28 14:52:17 +0530

    added about me

--- src/components/AboutContext.tsx
+++ src/components/AboutContext.tsx
@@ -2,9 +2,9 @@
 export default function About() {
   return (
     <>
-    <div className="sm:w-3/4 w-1/2">
+    <div className="w-3/4 md:w-1/2 flex flex-col gap-7">
     <p>I didn’t enter technology through tutorials or certifications.
-       I entered it by breaking things.</p>
+    <br/> I entered it by breaking things.</p>
     
     <p>At the age of 12, I got my first computer — a Dell desktop with an Intel Dual Core processor, 2GB DDR2 RAM, a 512GB hard drive, and Windows XP. When the system started beeping, I didn’t panic — I opened it. I disassembled the CPU just to understand why it was failing. That curiosity stayed with me.</p>
 
@@ -14,7 +14,7 @@ export default function About() {
 
     <p>When Kali Linux finally ran, it wasn’t the hacking tools that hooked me — it was the visibility into the system. Tools like Wireshark, Nmap, Burp Suite introduced me to how networks and systems actually behave. I didn’t understand everything, but I knew this was the direction.</p>
 
-    <p>During my +1 and +2, most people barely touched computers. I lived inside them.</p>
+    <p>During my +1 and +2, I barely touched computers. But I lived inside them.</p>
 
     <p>By the time I entered my B.Tech in Computer Science, my focus shifted again — from security curiosity to understanding systems internally: how operating systems work, how backend services communicate, how performance and scale emerge from design choices. Getting a Mac with the M1 chip only deepened this interest — different architecture, different trade-offs, same curiosity.</p>
 
@@ -22,6 +22,7 @@ export default function About() {
 
     <p>I currently work on backend development and am actively building my foundation in web technologies and AWS. I’m at the beginning of serious system design work — and that excites me. I’m not chasing trends; I’m chasing understanding.</p>
 
+    <div className="flex flex-col gap-2">
     <h2>My Philosophy</h2>
     <p>I believe good systems are not built by accident.
 They are built by engineers who:</p>
@@ -30,11 +31,15 @@ They are built by engineers who:</p>
         <li>Respect constraints like performance, scale, and failure</li>
         <li>Treat logs, metrics, and failures as first-class signals</li>
     </ul>
+    </div>
 
     <p>My long-term goal is to become the go-to infrastructure and system architecture engineer — someone trusted to design, debug, and scale critical systems — and to work on large-scale cloud platforms like AWS.</p>
 
+    <div>
     <p>And this is ME</p>
+
     <p>Prashanth Chowdary</p>
+    </div>
     </div>
     </>
   )
--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,9 +1,11 @@
 import AboutContext from '../components/AboutContext';
+import Nav from '../components/Nav';
 export default function About() {
   return (
     <>
-    <div className='flex flex-col items-center justify-center'>      
-    <h1 className='text-2xl font-bold'>ABOUT ME</h1>
+    <Nav/>
+    <div className='flex flex-col items-center justify-center mb-10'>      
+    <h1 className='text-6xl font-bold mt-20 mb-10'>ABOUT ME</h1>
     <AboutContext/>
     </div>
     </>
